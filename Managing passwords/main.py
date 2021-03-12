2c5d5de50a27303962911bef6b91e54ec2659a78
p.sz1 <p.sz1@op.pl>
2021-03-12 13:34:14 +0100

    Initialize password managing task

---
+++ Managing passwords/main.py
@@ -0,0 +1,32 @@
+import getpass
+import hashlib
+import os
+import binascii
+
+#password1 = getpass.getpass(prompt='Password: ', stream=None) ??
+
+
+def hash_password(password):
+    """Hash a password for storing"""
+    salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
+    pwdhash = hashlib.pbkdf2_hmac('sha512', password.encode('utf-8'),
+                                salt, 100000)
+    pwdhash = binascii.hexlify(pwdhash)
+    return (salt + pwdhash).decode('ascii')
+
+def verify_password(stored_password, provided_password):
+    """Verify stored password with one provided by user"""
+    salt = stored_password[:64]
+    stored_password = stored_password[64:]
+    pwdhash = hashlib.pbkdf2_hmac('sha512',
+                                  provided_password.encode('utf-8'),
+                                  salt.encode('ascii'),
+                                  100000)
+    pwdhash = binascii.hexlify(pwdhash).decode('ascii')
+    return pwdhash == stored_password
+
+password1 = getpass.getpass()
+password2 = input('Powtórz hasło: ')
+
+if password1 != password2:
+    print('Błąd!')
