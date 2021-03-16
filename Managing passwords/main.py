7c067f1b201885a0817b70eec527f2c9f35ed7da
p.sz1 <p.sz1@op.pl>
2021-03-16 21:39:23 +0100

    added comments

--- Managing passwords/main.py
+++ Managing passwords/main.py
@@ -1,9 +1,6 @@
 from getpass import getpass
 from src.Database import ManageDatabase
-from src.ManagePassword import ManagePassword
 
-db = ManageDatabase()
-db.create_table()
 
 provided_password = input('Podaj hasło: ')
 
@@ -13,12 +10,7 @@ while True:
     else:
         print('Hasła nie są takie same!')
 
-p = ManagePassword()
-
-# password = p.hash_password(provided_password)
-# print(p.verify_password('6456ae47d19fe64c0b33325ca5955829eb755520f5c54108ccb55c48a99ecbe101051b4a62cb0a2724245e14202dd2c21cbdeebddbe71782c930f658288e598b79d1c0da296435e3c02f1a1ae61cf2cb934ac228253e837322a37f67ed1241c7', provided_password))
-# print(provided_password)
-# print(password)
-
+db = ManageDatabase()
+db.create_table()
 db.insert_password(provided_password)
 
--- Managing passwords/src/Database.py
+++ Managing passwords/src/Database.py
@@ -1,67 +1,78 @@
 import sqlite3
 from src.ManagePassword import ManagePassword
+import logging
 
 class ManageDatabase():
     """Class that manage database containing passwords"""
 
+    logger = logging.getLogger('database')
+    logging.basicConfig(level=logging.DEBUG)
+
     def __init__(self):
         self.m = ManagePassword()
-
 
     def __connect_with_database(self):
         return sqlite3.connect('database.db')
 
     def create_table(self):
+        """Checking if table for passwords exists and if not, creates one"""
+
         conn = self.__connect_with_database()
         cursor = conn.cursor()
         try:
+            #
             cursor.execute('''SELECT count(*) FROM sqlite_master WHERE type='table' AND name='passwords' ''')
 
             if cursor.fetchone()[0] == 0:
                 cursor.execute("""CREATE TABLE passwords (password text )""")
                 conn.commit()
-                print('Table succesfully created')
+                self.logger.info('Table succesfully created')
             else:
-                print('Table already exists')
+                self.logger.info('Table already exists')
 
         except Exception as e:
-            print('Error ocurred while creating database table \nError: %s' % e)
+            self.logger.info('Error ocurred while creating database table \nError: %s' % e)
 
         conn.close()
 
 
     def __check_whether_password_exists(self, provided_password):
+        """Checking if password already is in database"""
+
         conn = self.__connect_with_database()
         cursor = conn.cursor()
         try:
+            # gets all records from database into tuple
             rows = cursor.execute('''SELECT * FROM passwords''').fetchall()
             if len(rows) != 0:
                 for row in rows[0]:
+                    # compare every record from database with user input
                     if self.m.verify_password(row, provided_password):
                         return True
                 return False
             else:
                 return False
         except Exception as e:
-            print('Error ocurred while checking if password is already in database \nError message: %s' % e)
-
+            self.logger.info('Error ocurred while checking if password is already in database \nError message: %s' % e)
         conn.close()
 
 
     def insert_password(self, password):
+        """Insert password into database if it's unique"""
+
         conn = self.__connect_with_database()
         cursor = conn.cursor()
         try:
             if self.__check_whether_password_exists(password):
-                print('This password already exists in database')
+                self.logger.info('This password already exists in database')
             else:
+                # inserting password into database
                 password = self.m.hash_password(password)
-                print(password)
                 query = """INSERT INTO passwords (password) VALUES (?)"""
                 cursor.execute(query, (password,))
                 conn.commit()
-                print('Password succesfully added to database')
+                self.logger.info('Password succesfully added to database')
         except Exception as e:
-            print('Error ocurred while inserting password into database table \nError message: %s' % e)
+            self.logger.info('Error ocurred while inserting password into database table \nError message: %s' % e)
 
         conn.close()
