50880ca11afa4b74fe569539cf3bd303e93f99f3
p.sz1 <p.sz1@op.pl>
2021-03-24 17:23:27 +0100

    symmetric templates

--- API encryption/main.py
+++ API encryption/main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/symmetric/key")
+def get_key():
+    """Returns a randomly generated symmetric key in the form of HEX"""
+    #return key
+
+@app.post("/symmetric/key")
+def set_key():
+    """Sets the symmetric key provided in the form of HEX in request on the server"""
+
+@app.post("/symmetric/encode")
+def symmetric_encode(message: str):
+    """Sends a message and as a result returns it encrypted"""
+
+@app.post("/symmetric/decode")
+def symmetric_decode(message: str):
+    """Sends a message and as a result returns it decrypted"""
