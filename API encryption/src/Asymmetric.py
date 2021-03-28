a0a02d61f56efceba9c5eed7809d7d896bbce353
p.sz1 <p.sz1@op.pl>
2021-03-28 14:07:27 +0200

    added asymmetric method for creating private and public keys

--- API encryption/src/Asymmetric.py
+++ API encryption/src/Asymmetric.py
@@ -1,40 +1,21 @@
-# from fastapi import FastAPI
-# from cryptography.fernet import Fernet
-#
-# app = FastAPI()
-#
-# class Asymmetric:
-#
-#     @app.get("/asymmetric/key")
-#     def get_asymmetric_key(self):
-#         """Returns new public and private key in the form of HEX (in JSON as dict) and sets it on the server"""
-#
-#
-#     @app.get("/asymmetric/key/ssh")
-#     def get_ssh_key(self):
-#         """Returns public and private key in HEX format saved in OpenSSH format"""
-#
-#
-#     @app.post("/asymmetric/key")
-#     def set_asymmetric_key(self):
-#         """Sets the public and private key in the form of HEX (in JSON as dict)"""
-#
-#
-#     @app.post("/asymmetric/verify")
-#     def verify_asymmetric_key(self):
-#         """Using the currently set private key, signs the message and returns it with the signed one"""
-#
-#
-#     @app.post("/asymmetric/sign")
-#     def verify_asymmetric_public_key(self):
-#         """Using the currently set public key, verify if the message was encrypted with it"""
-#
-#
-#     @app.post("/asymmetric/encode")
-#     def asymmetric_encode(self, message: str):
-#         """Sends a message and as a result returns it encrypted"""
-#
-#
-#     @app.post("/asymmetric/decode")
-#     def asymmetric_decode(self, message: str):
-#         """Sends a message and as a result returns it decrypted"""
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+class Asymmetric:
+
+
+    keys = {
+        "privateKey": "",
+        "publicKey": ""
+    }
+
+    def __update_keys(self, privateKey):
+        self.keys["privateKey"] = privateKey.private_bytes(encoding=serialization.Encoding.PEM,
+                                                           format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                           encryption_algorithm=serialization.NoEncryption())
+        self.keys["publicKey"] = hex(privateKey.public_key().public_bytes(encoding=serialization.Encoding.PEM,
+                                                                      format=serialization.PublicFormat.SubjectPublicKeyInfo))
+
+    def create_private_and_public_key(self):
+        self.__update_keys(rsa.generate_private_key(public_exponent=65537, key_size=2048))
+        return self.keys
--- API encryption/src/Routes.py
+++ API encryption/src/Routes.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+
+from src.Asymmetric import Asymmetric
 from src.Symmetric import Symmetric
 
 
 app = FastAPI()
+
+#symmetric
 
 symmetric = Symmetric()
 
@@ -28,3 +32,42 @@ def symmetric_encode(message: str):
 def symmetric_decode(message: str):
     """Sends a message and as a result returns it decrypted"""
     return symmetric.decode(message)
+
+#asymmetric
+
+asymmetric = Asymmetric()
+
+@app.get("/asymmetric/key")
+def get_asymmetric_key():
+    """Returns new public and private key in the form of HEX (in JSON as dict) and sets it on the server"""
+    return asymmetric.create_private_and_public_key()
+
+
+@app.get("/asymmetric/key/ssh")
+def get_ssh_key():
+    """Returns public and private key in HEX format saved in OpenSSH format"""
+
+
+@app.post("/asymmetric/key")
+def set_asymmetric_key():
+    """Sets the public and private key in the form of HEX (in JSON as dict)"""
+
+
+@app.post("/asymmetric/verify")
+def verify_asymmetric_key():
+    """Using the currently set private key, signs the message and returns it with the signed one"""
+
+
+@app.post("/asymmetric/sign")
+def verify_asymmetric_public_key():
+    """Using the currently set public key, verify if the message was encrypted with it"""
+
+
+@app.post("/asymmetric/encode")
+def asymmetric_encode(message: str):
+    """Sends a message and as a result returns it encrypted"""
+
+
+@app.post("/asymmetric/decode")
+def asymmetric_decode(message: str):
+    """Sends a message and as a result returns it decrypted"""
