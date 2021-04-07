d0c49e641d2a1d493d754f9cfc2c04c82037aad5
p.sz1 <p.sz1@op.pl>
2021-04-07 13:30:18 +0200

    minor changes in asymmetric class

--- API encryption/src/Asymmetric.py
+++ API encryption/src/Asymmetric.py
@@ -1,3 +1,4 @@
+from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
@@ -10,30 +11,76 @@ class Asymmetric:
     logger = logging.getLogger('database')
     logging.basicConfig(level=logging.DEBUG)
 
-    keys = {
-        "privateKey": "",
-        "publicKey": ""
-    }
+    def __init__(self):
+        self.keys = {
+            "privateKey": None,
+            "publicKey": None
+        }
 
-    def __update_keys(self, privateKey):
+        self.pemKeys = {
+            "privateKey": None,
+            "publicKey": None
+        }
+
+        self.hexKeys = {
+            "privateKey": "",
+            "publicKey": ""
+        }
+
+    def __update_keys(self):
         """Updates key values in dictionary"""
 
-        self.keys["privateKey"] = privateKey.private_bytes(encoding=serialization.Encoding.PEM,
-                                                           format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                           encryption_algorithm=serialization.NoEncryption()).hex()
-        self.keys["publicKey"] = privateKey.public_key().public_bytes(encoding=serialization.Encoding.PEM,
-                                                                      format=serialization.PublicFormat.SubjectPublicKeyInfo).hex()
+        self.keys["privateKey"] = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+        )
+
+        self.keys["publicKey"] = self.keys["privateKey"].public_key()
+
         self.logger.info("Keys updated succesfully")
+
+        self.__update_pem_keys()
+
+        self.logger.info("Pem keys updated succesfully")
+
+        self.__update_hex_keys()
+
+        self.logger.info("Hex keys updated succesfully")
+
+    def __update_pem_keys(self):
+        """Convert keys to pem format"""
+
+        self.pemKeys["privateKey"] = self.keys["privateKey"].private_bytes(encoding=serialization.Encoding.PEM,
+                                                                   format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                                                   encryption_algorithm=serialization.NoEncryption())
+        self.pemKeys["publicKey"] = self.keys["privateKey"].public_key().public_bytes(encoding=serialization.Encoding.PEM,
+                                                                              format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+
+    def __update_hex_keys(self):
+        """Convert keys to hex format"""
+
+        self.hexKeys["privateKey"] = self.pemKeys["privateKey"].hex()
+        self.hexKeys["publicKey"] = self.pemKeys["publicKey"].hex()
+
+    # def __unhex_keys(self):
+    #     """Convert keys from hex to binary format"""
+    #
+    #     self.pemKeys["privateKey"] = self.pemKeys["privateKey"].decode(hex)
+    #     self.pemKeys["publicKey"] = self.pemKeys["publicKey"].decode(hex)
 
 
     def create_private_and_public_key(self):
         """"Generates private key and passes it to method that updates both keys values"""
 
-        self.__update_keys(rsa.generate_private_key(public_exponent=65537, key_size=2048))
-        return self.keys
+        self.__update_keys()
+
+        return self.hexKeys
 
     def set_key_values(self, privateKey, publicKey):
         """Takes the private key and based on it's value generates public one"""
+
         self.keys["privateKey"] = privateKey
         self.keys["publicKey"] = publicKey
 
@@ -43,6 +90,8 @@ class Asymmetric:
 
     def sign_message(self, message):
         """Takes message and signs it using private key"""
+
+        self.__update_keys()
 
         return self.keys["privateKey"].sign(
             message,
--- API encryption/src/Routes.py
+++ API encryption/src/Routes.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-
 from src.Asymmetric import Asymmetric
 from src.Symmetric import Symmetric
 
@@ -11,25 +10,25 @@ app = FastAPI()
 symmetric = Symmetric()
 
 @app.get("/symmetric/key/")
-def symmetric_get_key():
+async def symmetric_get_key():
     """Returns a randomly generated symmetric key in the form of HEX"""
     return symmetric.create_key()
 
 
 @app.post("/symmetric/key/")
-def symmetric_set_key(key: str):
+async def symmetric_set_key(key: str):
     """Sets the symmetric key provided in the form of HEX in request on the server"""
     symmetric.set_key(key)
 
 
 @app.post("/symmetric/encode/")
-def symmetric_encode(message: str):
+async def symmetric_encode(message: str):
     """Sends a message and as a result returns it encrypted"""
     return symmetric.encode(message)
 
 
 @app.post("/symmetric/decode/")
-def symmetric_decode(message: str):
+async def symmetric_decode(message: str):
     """Sends a message and as a result returns it decrypted"""
     return symmetric.decode(message)
 
@@ -38,41 +37,41 @@ def symmetric_decode(message: str):
 asymmetric = Asymmetric()
 
 @app.get("/asymmetric/key")
-def get_asymmetric_key():
+async def get_asymmetric_key():
     """Returns new public and private key in the form of HEX (in JSON as dict) and sets it on the server"""
 
     return asymmetric.create_private_and_public_key()
 
 
 @app.get("/asymmetric/key/ssh")
-def get_ssh_key():
+async def get_ssh_key():
     """Returns public and private key in HEX format saved in OpenSSH format"""
 
 
 @app.post("/asymmetric/key")
-def set_asymmetric_key(privateKey: str, publicKey: str):
+async def set_asymmetric_key(privateKey: str, publicKey: str):
     """Sets the public and private key in the form of HEX (in JSON as dict)"""
 
-    return asymmetric.set_key_values(privateKey, publicKey)
+    return asymmetric.set_key_values(privateKey)
 
 
 @app.post("/asymmetric/sign")
-def sign_message(message: str):
+async def sign_message(message: str):
     """Using the currently set private key, signs the message and returns it with the signed one"""
 
     return asymmetric.sign_message(message)
 
 
 @app.post("/asymmetric/verify")
-def verify_asymmetric_public_key():
+async def verify_asymmetric_public_key():
     """Using the currently set public key, verify if the message was encrypted with it"""
 
 
 @app.post("/asymmetric/encode")
-def asymmetric_encode(message: str):
+async def asymmetric_encode(message: str):
     """Sends a message and as a result returns it encrypted"""
 
 
 @app.post("/asymmetric/decode")
-def asymmetric_decode(message: str):
+async def asymmetric_decode(message: str):
     """Sends a message and as a result returns it decrypted"""
