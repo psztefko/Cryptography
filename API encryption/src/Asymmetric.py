5b9bcf190da6492e9ba99d774abc8474ec92fa52
p.sz1 <p.sz1@op.pl>
2021-04-08 22:47:18 +0200

    everything is working as intended

--- API encryption/src/Asymmetric.py
+++ API encryption/src/Asymmetric.py
@@ -12,7 +12,7 @@ import logging
 
 class Asymmetric:
 
-    logger = logging.getLogger('database')
+    logger = logging.getLogger('asymmetric')
     logging.basicConfig(level=logging.DEBUG)
 
     privateKey = None
@@ -67,13 +67,13 @@ class Asymmetric:
         return {"privatePem": privateHex, "publicPem": publicHex}
 
 
-    def create_private_and_public_key(self):
+    def create_private_and_public_key(self) -> dict:
         """"Generates private key and passes it to method that updates both keys values"""
 
         return self.__generate_hex_keys()
 
 
-    def set_key_values(self, privateKey, publicKey):
+    def set_key_values(self, privateKey, publicKey) -> dict:
         """Takes the private key and based on it's value generates public one"""
 
         self.privateKey = privateKey
@@ -84,7 +84,7 @@ class Asymmetric:
         return self.keys
 
 
-    def get_keys_in_ssh(self):
+    def get_keys_in_ssh(self) -> dict:
         """Returns keys in OpenSSH format"""
 
         privateSSH = self.privateKey.private_bytes(
@@ -103,7 +103,7 @@ class Asymmetric:
             'publicSSH': publicSSH
         }
 
-    def sign_message(self, message):
+    def sign_message(self, message) -> bytes:
         """Takes message and signs it using private key"""
 
         prehashed = hashlib.sha256(message.encode('ascii')).hexdigest()
@@ -122,40 +122,45 @@ class Asymmetric:
             self.logger.error("Private key value not set")
 
 
-    def verify_message(self, message, signature):
+    def verify_message(self, message: str, signature: str) -> bool:
         """Using the currently set public key, verifies if the message was encrypted with it"""
 
+        decodedSignature = base64.b64decode(signature)
         prehashedMessage = hashlib.sha256(message.encode('ascii')).hexdigest()
-        decodedSignature = base64.b64encode(signature)
 
         try:
-            return self.publicKey.verify(
+            self.publicKey.verify(
                 decodedSignature,
                 bytes(prehashedMessage.encode('ascii')),
                 padding.PSS(
                     mgf=padding.MGF1(hashes.SHA256()),
-                    salt_length=padding.PSS.MAX_LENGTH
-                ),
-                hashes.SHA256()
-            )
+                    salt_length=padding.PSS.MAX_LENGTH),
+                hashes.SHA256())
         except InvalidSignature:
             self.logger.error("Invalid Signature")
-            return "Invalid Signature"
+            return False
+        else:
+            self.logger.info("Signature verified successfully")
+            return True
 
-    def encrypt_message(self, message):
+
+    def encrypt_message(self, message: str) -> bytes:
         """Gets a message and returns it encrypted"""
 
-        return base64.b64encode(self.publicKey.encrypt(
-            message.encode(),
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-        ))
+        try:
+            return base64.b64encode(self.publicKey.encrypt(
+                message.encode(),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            ))
+        except:
+            self.logger.error("Error ocurred while encrypting message")
 
 
-    def decrypt_messege(self, message):
+    def decrypt_messege(self, message: str) -> bytes:
         """Gets encrypted message and returns it decrypted"""
 
         return self.privateKey.decrypt(
