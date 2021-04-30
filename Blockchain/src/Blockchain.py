9b14e062f4f64d2d6ff5581b4818787c265eb502
p.sz1 <p.sz1@op.pl>
2021-04-30 16:30:47 +0200

    created Blockchain class

---
+++ Blockchain/src/Blockchain.py
@@ -0,0 +1,19 @@
+import time
+import Block
+
+class Blockchain:
+
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), '0')
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
